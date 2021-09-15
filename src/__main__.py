15f0456c13e4894cffa709c0ff7aaada63dd2f09
Edwin Guamushig <edwin.paul.73@gmail.com>
2021-09-15 11:47:38 -0500

    ignore node modules and install jscpd if not exists

--- src/__main__.py
+++ src/__main__.py
@@ -10,9 +10,10 @@ def get_terminal_variables():
 
 
 def execute_jscpd(path_folder):
-    complete_command = 'jscpd {} --ignore "**/*.json,**/*.yml" --reporters json --output ./report/ '.format(path_folder)
-    os.system('npm i jscpd -g')
-    os.system(complete_command)
+    complete_jscpd_command = 'jscpd {} --ignore "**/*.json,**/*.yml,**/node_modules/**" --reporters json --output ' \
+                       './report/ '.format(path_folder)
+    os.system('npm list -g jscpd || npm i -g jscpd@3.3.26')
+    os.system(complete_jscpd_command)
 
 
 def read_reporter_json():
@@ -20,13 +21,11 @@ def read_reporter_json():
     with open(json_reporter_path) as json_file:
         json_object = json.load(json_file)
         json_file.close()
-    total_lines = json_object['statistics']['total']['lines']
     total_percentage = json_object['statistics']['total']['percentage']
-    total_duplicated_lines = json_object['statistics']['total']['duplicatedLines']
-    print(total_percentage, total_lines, total_duplicated_lines)
+    print('percentage total', total_percentage)
 
 
 if __name__ == '__main__':
-    path = get_terminal_variables()
-    execute_jscpd(path)
+    project_path = get_terminal_variables()
+    execute_jscpd(project_path)
     read_reporter_json()
