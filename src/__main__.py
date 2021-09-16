a75f781573e8736734da60a6f6cbc138df6c2ac8
Edwin Guamushig <edwin@tinkin.one>
2021-09-16 17:34:59 -0500

    changes

--- src/__main__.py
+++ src/__main__.py
@@ -3,29 +3,30 @@ import os
 import json
 
 
-def get_terminal_variables():
-    console_arguments = sys.argv
-    path_analyze_folder = console_arguments[1]
-    return path_analyze_folder
+def get_directory_path_to_analyze():
+    if sys.argv[1]:
+        directory_path_to_analyze = sys.argv[1]
+        return directory_path_to_analyze
+    print('No ha enviado el directorio a ser analizado')
 
 
-def execute_jscpd(path_folder):
-    complete_jscpd_command = 'jscpd {} --ignore "**/*.json,**/*.yml,**/node_modules/**" --reporters json --output ' \
-                       './report/ '.format(path_folder)
+
+def calculate_code_duplication(directory_path):
+    complete_jscpd_command = 'jscpd {} --silent --ignore "**/*.json,**/*.yml,**/node_modules/**" --reporters json' \
+                       '--output  ./report/ '.format(directory_path)
     os.system('npm list -g jscpd || npm i -g jscpd@3.3.26')
     os.system(complete_jscpd_command)
+    get_total_percentage_repeat_code()
 
 
-def read_reporter_json():
+def get_total_percentage_repeat_code():
     json_reporter_path = './report/jscpd-report.json'
     with open(json_reporter_path) as json_file:
         json_object = json.load(json_file)
-        json_file.close()
     total_percentage = json_object['statistics']['total']['percentage']
     return total_percentage
 
 
 if __name__ == '__main__':
-    project_path = get_terminal_variables()
-    execute_jscpd(project_path)
-    read_reporter_json()
+    directory_path_to_analyze = get_directory_path_to_analyze()
+    calculate_code_duplication(directory_path_to_analyze)
