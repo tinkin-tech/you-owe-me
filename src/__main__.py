36ea3343db3a2d42f842512c4a645b34fff8408e
Edwin Guamushig <edwin.paul.73@gmail.com>
2021-09-20 18:19:14 -0500

    get coverage code

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,7 @@ import sys
 import os
 import re
 import csv
+import subprocess
 from datetime import datetime, timedelta
 from constants.config import load_environment_variables
 
@@ -30,25 +31,44 @@ def check_duplicate_code_commits(directory_path):
     dates = get_measurement_dates_between_intervals()
     last_commit_analyzed = ''
     for commit_date in dates:
-        commit_id = os.popen('cd {} && git checkout master --quiet && git rev-list --before {} -1 master'
-                             .format(directory_path, commit_date)).read()
+        get_commit_id = 'cd {} && git checkout master --quiet && git rev-list --before {} ' \
+                        '-1 master'.format(directory_path, commit_date)
+        commit_id = subprocess.check_output(get_commit_id, shell=True).decode('utf-8')
         if commit_id == last_commit_analyzed:
             print('Repeated commit')
             break
         last_commit_analyzed = commit_id
-        os.system('cd {} && git checkout {} --quiet'.format(directory_path, commit_id.strip()))
-        data_to_write_csv = {'date': commit_date, 'duplication': calculate_code_duplication(directory_path)}
+        switch_to_commit = 'cd {} && git checkout {} --quiet'.format(directory_path, commit_id.strip())
+        subprocess.run(switch_to_commit, shell=True)
+        data_to_write_csv = {'date': commit_date, 'duplication': get_percentage_duplicate_code(directory_path)}
         write_csv_report(data_to_write_csv)
 
 
-def calculate_code_duplication(directory_path):
-    regex_files = environment_variables['REGEX_IMPLEMENTATION_FILES']
-    complete_jscpd_command = 'jscpd {} --silent --ignore "**/*.json,**/*.yml,**/node_modules/**" ' \
-                             '--pattern "{}"'.format(directory_path, regex_files)
-    os.system('npm list -g jscpd > /dev/null || npm i -g jscpd@3 --silent')
-    jscpd_response = os.popen(complete_jscpd_command).read()
-    total_percentage_duplicated = re.findall('\\d+(?:\\.\\d+)?%', jscpd_response.strip())[0]
+def get_percentage_duplicate_code(directory_path):
+    regex_implementation_files = environment_variables['REGEX_IMPLEMENTATION_FILES']
+    get_code_duplication = 'jscpd {} --silent --ignore "**/*.json,**/*.yml,**/node_modules/**" ' \
+                           '--pattern "{}"'.format(directory_path, regex_implementation_files)
+    subprocess.run('npm list -g jscpd || npm i -g jscpd@3', shell=True, stdout=subprocess.DEVNULL,
+                   stderr=subprocess.STDOUT)
+    output = subprocess.check_output(get_code_duplication, shell=True).decode('utf-8')
+    # the regular expression find the numbers with percentage
+    total_percentage_duplicated = re.findall('\\d+(?:\\.\\d+)?%', output.strip())[0]
     return total_percentage_duplicated
+
+
+def get_code_coverage_report(directory_path):
+    regex_implementation_files = environment_variables['REGEX_IMPLEMENTATION_FILES']
+    generate_coverage_report = 'cd {} && npx jest --coverage ' \
+                               '--coverageReporters="json-summary" --collectCoverageFrom={}' \
+                               ''.format(directory_path, regex_implementation_files)
+    get_head_coverage_report = 'cd {} && cat coverage/coverage-summary.json | head -1'.format(directory_path)
+    subprocess.run(generate_coverage_report, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    output = subprocess.check_output(get_head_coverage_report, shell=True).decode('utf-8')
+    # the regular expression looks for the deepest value nested inside a json, and returns it (return information
+    summary_lines_code = re.findall('(?<=\{)\s*[^{]*?(?=[\}])', output.strip())[0]
+    percentage_code_coverage = summary_lines_code.split(',')[3]
+    print(int(filter(str.isdigit, percentage_code_coverage)))
+    print(type(percentage_code_coverage))
 
 
 def write_csv_report(data_to_write):
@@ -65,4 +85,5 @@ def write_csv_report(data_to_write):
 
 
 if __name__ == '__main__':
-    check_duplicate_code_commits(get_directory_path_to_analyze())
+    # check_duplicate_code_commits(get_directory_path_to_analyze())
+    get_code_coverage_report(get_directory_path_to_analyze())
