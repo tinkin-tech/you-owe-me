27b4bf73857dd02726b3d578631a1a7364fb88e3
Quezma <quezma@iMac-de-Quezma.local>
2021-09-22 15:35:13 -0500

    Create function for get element from list by index and create guard clause

--- src/__main__.py
+++ src/__main__.py
@@ -3,29 +3,44 @@ import subprocess
 import re
 
 
+def have_more_than_one_element(list):
+    return len(list) > 1
+
+
+def get_element_by_index(list, index, default_value):
+    try:
+        return list[index]
+    except IndexError:
+        return default_value
+
+
 def get_directory_path_to_analyze():
-    if len(sys.argv) > 1:
+    if have_more_than_one_element(sys.argv):
         return sys.argv[1]
     raise Exception("The directory to be analyzed must be passed as argument")
 
 
 def install_debt_report_dependencies():
     subprocess.run(
-        "npm list -g jscpd || npm i -g jscpd@3.3.26",
+        "npm list -g jscpd || npm i -g jscpd@latest",
         shell=True,
         stdout=subprocess.DEVNULL,
     )
 
 
 def get_code_duplication_percentage(directory_path):
-    code_duplication_report = subprocess.check_output(
-        "jscpd {} --silent --ignore  "
-        '"**/*.json,**/*.yml,**/node_modules/**"'.format(directory_path),
-        shell=True,
-    ).decode("utf-8").strip()
-    return re.findall(
-        "\\d+(?:\\.\\d+)?%", code_duplication_report
-    )[0]
+    code_duplication_report = (
+        subprocess.check_output(
+            "jscpd '{}' --silent --ignore  "
+            '"**/*.json,**/*.yml,**/node_modules/**"'.format(directory_path),
+            shell=True,
+        )
+        .decode("utf-8")
+        .strip()
+    )
+    return get_element_by_index(
+        re.findall("\\d+(?:\\.\\d+)?%", code_duplication_report), 0, "0%"
+    )
 
 
 def generate_debt_report():
@@ -34,9 +49,10 @@ def generate_debt_report():
         """
     Report Type      | Result
     -----------------|-----------
-    Code Duplication | %s
-    """
-        % get_code_duplication_percentage(get_directory_path_to_analyze())
+    Code Duplication | {}
+    """.format(
+            get_code_duplication_percentage(get_directory_path_to_analyze())
+        )
     )
 
 
