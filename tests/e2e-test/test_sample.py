a511e3e2144a735c407a9b656590ebaf066f3ad7
Sathors <sathors@free.fr>
2021-09-23 14:56:50 -0500

    Make all absolute imports work when using pytest, also ignore all python things in the gitignore.

--- tests/e2e-test/test_sample.py
+++ tests/e2e-test/test_sample.py
@@ -1,9 +1,8 @@
-import pytest
-import __main__
+from src.__main__ import generate_debt_report
 
 
 def test_directory_path():
-    assert __main__.generate_debt_report('~/Desktop/tinkin/python/you-owe-me/tests/repository-fixture/') == """
+    assert generate_debt_report('~/Desktop/tinkin/python/you-owe-me/tests/repository-fixture/') == """
                                                                                                     Report Type      | Result
                                                                                                     -----------------|-----------
                                                                                                     Code Duplication | 40%
