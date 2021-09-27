0166f0af67039309e1fb31ed7c33cdc54630f2d0
Edwin Guamushig <edwin.paul.73@gmail.com>
2021-09-27 10:23:17 -0500

    format

--- tests/e2e/test_code_duplicate_percentage.py
+++ tests/e2e/test_code_duplicate_percentage.py
@@ -6,8 +6,8 @@ def test_code_duplication_percentage():
     assert (
         generate_debt_report(
             "tests/demo-repository",
-            datetime.strptime('2021-09-24', "%Y-%m-%d"),
-            datetime.strptime('2021-09-27', "%Y-%m-%d"),
+            datetime.strptime("2021-09-24", "%Y-%m-%d"),
+            datetime.strptime("2021-09-27", "%Y-%m-%d"),
             int(1),
         )
         == """
