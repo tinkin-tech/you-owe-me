6de969c4a1f974ae57dc872010fa38a220b217ae
Edwin Guamushig <edwin.paul.73@gmail.com>
2021-09-27 09:27:53 -0500

    get percentage code duplication by date

--- src/__main__.py
+++ src/__main__.py
@@ -2,12 +2,14 @@ import sys
 import subprocess
 import re
 from os import path
+from datetime import timedelta
+from constants.config import load_environment_variables
 
 REGEX_TO_FIND_PERCENTAGE_NUMBER = "\\d+(?:\\.\\d+)?%"
 
 
-def has_more_than_one_element(dynamic_list):
-    return len(dynamic_list) > 1
+def has_more_than_one_element(list_):
+    return len(list_) > 1
 
 
 def get_directory_path_to_analyze():
@@ -29,6 +31,25 @@ def install_debt_report_dependencies():
     )
 
 
+def get_dates_by_day_interval(start_date, end_date, interval_in_days):
+    dates_by_day_interval = []
+    while start_date <= end_date:
+        start_date = start_date + timedelta(days=interval_in_days)
+        dates_by_day_interval.append(start_date.strftime("%Y-%m-%d"))
+    return dates_by_day_interval
+
+
+def get_commit_by_date(directory_path, date):
+    return (
+        subprocess.check_output(
+            f"cd '{directory_path}' && git rev-list -1 --before {date} develop",
+            shell=True,
+        )
+        .decode("utf-8")
+        .strip()
+    )
+
+
 def get_code_duplication_percentage(directory_path):
     code_duplication_report = (
         subprocess.check_output(
@@ -44,14 +65,57 @@ def get_code_duplication_percentage(directory_path):
     ]
 
 
-def generate_debt_report(directory_path):
+def get_code_duplications_percentage_by_date(
+    directory_path, start_date, end_date, interval_in_days
+):
+    commit_analyzed = ""
+    code_duplications_percentage_by_date = []
+    for date in get_dates_by_day_interval(
+        start_date, end_date, interval_in_days
+    ):
+        commit_to_analyze = get_commit_by_date(directory_path, date)
+        if commit_to_analyze == commit_analyzed:
+            break
+        subprocess.run(
+            f"cd '{directory_path}' && git checkout {commit_to_analyze} --quiet --force",
+            shell=True,
+            stdout=subprocess.DEVNULL,
+            check=True,
+        )
+        commit_analyzed = commit_to_analyze
+        code_duplications_percentage_by_date.append({
+            'DATE': date,
+            'CODE_DUPLICATION': get_code_duplication_percentage(directory_path),
+        })
+    return code_duplications_percentage_by_date
+
+
+def generate_debt_report(
+    directory_path, start_date, end_date, interval_in_days
+):
     install_debt_report_dependencies()
-    return f"""
-    Report Type      | Result
-    -----------------|-----------
-    Code Duplication | {get_code_duplication_percentage(directory_path)}
+    header_report = """
+        Report Type      | Date      |   Result  
+        -----------------|-----------|----------\n
     """
+    body_report = ''
+    for code_duplication_percentage in get_code_duplications_percentage_by_date(
+        directory_path, start_date, end_date, interval_in_days
+    ):
+        body_report += f"""
+        Code Duplication | {code_duplication_percentage['DATE']} | {code_duplication_percentage['CODE_DUPLICATION']}
+        -----------------|------------|-----------
+        """
+    return f"{header_report} {body_report}"
 
 
 if __name__ == "__main__":
-    print(generate_debt_report(get_directory_path_to_analyze()))
+    env_variables = load_environment_variables()
+    print(
+        generate_debt_report(
+            get_directory_path_to_analyze(),
+            env_variables["START_DATE"],
+            env_variables["END_DATE"],
+            env_variables["INTERVAL_IN_DAYS"],
+        )
+    )
