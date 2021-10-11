e8e618952fd7f3ca3c2237c821840e1dd0b3c269
Edwin Guamushig <edwin@tinkin.one>
2021-10-11 08:40:30 -0500

    correct value on tests

--- tests/e2e/test_debt_report.py
+++ tests/e2e/test_debt_report.py
@@ -14,7 +14,7 @@ def test_debt_report():
         format_debt_report(debt_report_by_range)
         == "Date;Code Duplication;Implementation Lines;"
         "Test Lines; Total Lines\n"
-        "2021-09-28;33.33%;31;0;31\n"
-        "2021-09-29;30.77%;40;3;43\n"
-        "2021-09-30;9.43%;79;14;93"
+        "2021-09-28;40%;19;0;19\n"
+        "2021-09-29;33.33%;31;3;34\n"
+        "2021-09-30;30.77%;40;3;43"
     )
